8adfe545dbad9d812df3d49847c2774250552d16
Raoul de Villiers <raouldevilliers@gmail.com>
2014-07-14 21:31:28 +0200

    Updated factories.

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
   end
 
   factory :account_option do
+    id 1
     name "Test"
     cost 0
     users 2
@@ -13,38 +14,44 @@ FactoryGirl.define do
   end
 
   factory :account do
+    id 1
     name "Environmental NGO"
-    account_option
+    account_option_id 1
     active true
   end
 
   factory :type do
+    id 1
     description "Environmental"
-    account
+    account_id 1
   end
 
   factory :status do
+    id 1
     description "Open"
-    account
+    account_id 1
   end
 
   factory :priority do
+    id 1
     description "High"
-    account
+    account_id 1
   end
 
   factory :user do
+    id 1
     first_name "Test"
     last_name "User"
     phone "27834455556"
     password "mememe"
     password_confirmation "mememe"
-    account
+    account_id 1
     email { generate(:email) }
   end
 
   factory :group do
-    account
+    id 1
+    account_id 1
     name 'Cape Town'
     description 'All events in Cape Town'
     geo_info_file_name nil
@@ -54,13 +61,13 @@ FactoryGirl.define do
   end
 
   factory :update do
-    group
+    group_id 1
     detail "New message called 'Sort this out' created in group '#{FactoryGirl.build(:group).name}'."
   end
 
   factory :membership do
-    user
-    group
+    user_id 1
+    group_id 1
   end
 
   factory :incivent do
@@ -69,21 +76,21 @@ FactoryGirl.define do
     sequence(:description) {|n| "It was bad...#{n}" }
     latitude 60.555
     longitude 70.333
-    priority
-    type
-    status
-    user
-    group
+    priority_id 1
+    type_id 1
+    status_id 1
+    user_id 1
+    group_id 1
   end
 
   factory :task do
     sequence(:description) {|n| "Do something - #{n}!" }
     location '20 Corlett Drive, Johannesburg'
-    user
-    group
+    user_id 1
+    group_id 1
     raised_user_id 1
-    priority
-    status
+    priority_id 1
+    status_id 1
     due_date '2013-06-24'
     complete false
     latitude 60.0
@@ -94,8 +101,8 @@ FactoryGirl.define do
   factory :message do
     sequence(:description) {|n| "Sort this out...#{n}" }
     location '100 Corlett Drive, Johannesburg'
-    user
-    group
+    user_id 1
+    group_id 1
     latitude 60.555
     longitude 70.333
 
@@ -105,6 +112,4 @@ FactoryGirl.define do
     account_id 1
     recipient_email 'person@example.com'
   end
-
-
 end
