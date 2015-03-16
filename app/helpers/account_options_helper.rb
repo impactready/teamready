6bd05ca9119cefd3ab6f727a49a6b2ea251ba487
Raoul de Villiers <raouldevilliers@gmail.com>
2015-03-16 15:49:41 +0200

    Cleanup and changes for the new signup page.

--- app/helpers/account_options_helper.rb
+++
@@ -1,2 +0,0 @@
-module AccountOptionsHelper
-end
--- app/helpers/static_pages_helper.rb
+++
@@ -1,7 +0,0 @@
-module StaticPagesHelper
-
-	def account_option_param(string)
-		new_account_path + "?" + {:account_option => string }.to_param
-	end
-
-end
