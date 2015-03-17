6124fd2c9db87f754b45971fc3b1fc58d835c50d
Raoul de Villiers <raouldevilliers@gmail.com>
2015-03-17 10:52:06 +0200

    Changing the sendgrid settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,22 @@ Incivent_App::Application.configure do
 
   ### Mail now sent from Heroku -- SendGrid
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'http://impactready.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
   #config.action_mailer.delivery_method = :smtp
 
   # Defaults to Rails.root.join("public/assets")
