98f2d438391a867f7c03a82643b93f0a63cbab6e
Raoul de Villiers <raouldevilliers@gmail.com>
2015-03-24 09:07:15 +0200

    Trying the mobile inteface again for Geolocation.

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -26,5 +26,25 @@ $(function() {
       window.location = '/messages/new'
     });
 
+    get_coords();
+
 });
 
+get_coords = function() {
+    if (navigator.geolocation) {
+        if ($('.mobile-form input[id$="_latitude"]').length !== 0 && $('.mobile-form input[id$="_longitude"]').length !== 0 ) {
+            navigator.geolocation.getCurrentPosition(
+                function(position) {
+                    $('.mobile-form input[id$="_latitude"]').val(position.coords.latitude);
+                    $('.mobile-form input[id$="_longitude"]').val(position.coords.longitude);
+                },function(error) {
+                    alert('Could not obtain the coordinates from your device.');
+                }, {
+                    enableHighAccuracy: true,
+                    timeout: 5000,
+                    maximumAge: 60000
+                }
+            );
+        }
+    }
+}
