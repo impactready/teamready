ce6911abd8b64ab082b8e581cac1342b436bccb5
Raoul de Villiers <raouldevilliers@gmail.com>
2015-07-16 11:36:47 +0200

    Dumping assets.

--- db/migrate/20150707065035_add_usage_to_types.rb
+++ db/migrate/20150707065035_add_usage_to_types.rb
@@ -1,5 +1,5 @@
 class AddUsageToTypes < ActiveRecord::Migration
   def change
-    add_column :types, :usage, :string, null: false, default: ''
+    add_column :types, :usage, :string, null: false, default: 'Event category'
   end
 end
